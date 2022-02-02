cb54858abc499a85274e8b9d5c1cc1c5521ade0f
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-02-02 15:41:59 +0100

    lstm prepatation, code adjustments

--- Development/UserPrediction6DOF/runners.py
+++ Development/UserPrediction6DOF/runners.py
@@ -214,72 +214,10 @@ class LSTM():
         self.dataset_path = dataset_path
         self.results_path = results_path
         self.features = self.cfg['pos_coords'] + self.cfg['quat_coords'] + self.cfg['velocity'] + self.cfg['speed']
-        self.kf = KalmanFilter(dim_x=self.cfg['dim_x'], dim_z=self.cfg['dim_z'])
-        setattr(self.kf, 'x_pred', self.kf.x)
-
-        # First-order motion model: insert dt into the diagonal blocks of F
-        f = np.array([[1.0, self.dt], [0.0, 1.0]])
-        self.kf.F = block_diag(f, f, f, f, f, f, f)
-
-        # Inserts 1 into the blocks of H to select the measuremetns
-        np.put(self.kf.H, np.arange(0, self.kf.H.size, self.kf.dim_x + 2), 1.0)
-        self.kf.R *= self.cfg['var_R']
-        Q_pos = Q_discrete_white_noise(dim=2, dt=self.dt, var=self.cfg['var_Q_pos'], block_size=3)
-        Q_ang = Q_discrete_white_noise(dim=2, dt=self.dt, var=self.cfg['var_Q_ang'], block_size=4)
-        self.kf.Q = block_diag(Q_pos, Q_ang)
-
-    def reset(self):
-        logging.debug("Reset Kalman filter")
-        self.kf.x = np.zeros((self.cfg['dim_x'], 1))
-        self.kf.P = np.eye(self.cfg['dim_x'])
-
-    def lookahead(self):
-        self.kf.x_pred = np.dot(self.kf.F_lookahead, self.kf.x)
-
-    def run_single(self, trace_path, w):
-        # Adjust F depending on the lookahead time
-        f_l = np.array([[1.0, w], [0.0, 1.0]])
-        setattr(self.kf, 'F_lookahead', block_diag(f_l, f_l, f_l, f_l, f_l, f_l, f_l))
-
-        # Read trace from CSV file
-        df_trace = pd.read_csv(trace_path)
-        xs, covs, x_preds = [], [], []
-        zs = df_trace[self.features].to_numpy()
-        z_prev = np.zeros(7)
-        for z in zs:
-            sign_array = -np.sign(z_prev[3:]) * np.sign(z[3:])
-            sign_flipped = all(e == 1 for e in sign_array)
-            if sign_flipped:
-                logging.debug("A sign flip occurred.")
-                self.reset()
-            self.kf.predict()
-            self.kf.update(z)
-            self.lookahead()
-            xs.append(self.kf.x)
-            covs.append(self.kf.P)
-            x_preds.append(self.kf.x_pred)
-            z_prev = z
-
-        # Compute evaluation metrics
-        xs = np.array(xs).squeeze()
-        covs = np.array(covs).squeeze()
-        x_preds = np.array(x_preds).squeeze()
-        pred_step = int(w / self.dt)
-        eval = Evaluator(zs, x_preds[:, ::2], pred_step)
-        eval.eval_kalman()
-        metrics = np.array(list(eval.metrics.values()))
-        euc_dists = eval.euc_dists
-        ang_dists = np.rad2deg(eval.ang_dists)
-
-        return metrics, euc_dists, ang_dists
 
     def run(self):
-        logging.info("Kalman filter")
+        logging.info("LSTM (Long Short-Term Memory Network)")
         results = []
-
-        dists_path = os.path.join(self.results_path, 'distances')
-        if not os.path.exists(dists_path):
-            os.makedirs(dists_path)
 
         for trace_path in get_csv_files(self.dataset_path):
             basename = os.path.splitext(os.path.basename(trace_path))[0]
@@ -288,18 +226,22 @@ class LSTM():
             print("-------------------------------------------------------------------------")
             for w in self.pred_window:
                 logging.info("Prediction window = %s ms", w * 1e3)
-                self.reset()
 
-                metrics, euc_dists, ang_dists = self.run_single(trace_path, w)
-                np.save(os.path.join(dists_path,
-                                     'euc_dists_{}_{}ms.npy'.format(basename, int(w * 1e3))), euc_dists)
-                np.save(os.path.join(dists_path,
-                                     'ang_dists_{}_{}ms.npy'.format(basename, int(w * 1e3))), ang_dists)
-                result_single = list(np.hstack((basename, w, metrics)))
-                results.append(result_single)
+                # Read trace from CSV file
+                df_trace = pd.read_csv(trace_path)
+                zs = df_trace[self.coords].to_numpy()
+
+                pred_step = int(w / self.dt)
+                zs_shifted = zs[pred_step:, :]  # Assumption: LAT = E2E latency
+
+                # Compute evaluation metrics
+                eval = Evaluator(zs, zs_shifted, pred_step)
+                eval.eval_baseline()
+                metrics = np.array(list(eval.metrics.values()))
+                result_one_experiment = list(np.hstack((basename, w, metrics)))
+                results.append(result_one_experiment)
                 print("--------------------------------------------------------------")
 
-        # Save metrics
         df_results = pd.DataFrame(results, columns=['Trace', 'LAT', 'mae_euc', 'mae_ang',
                                                     'rmse_euc', 'rmse_ang'])
-        df_results.to_csv(os.path.join(self.results_path, 'res_kalman.csv'), index=False)
+        df_results.to_csv(os.path.join(self.results_path, 'res_lstm.csv'), index=False)
