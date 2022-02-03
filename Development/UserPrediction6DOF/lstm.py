6a37656e7329a2161081880d9176182f910fdf23
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-02-03 13:10:09 +0100

    lstm doc strings network architecture description

--- Development/UserPrediction6DOF/lstm.py
+++ Development/UserPrediction6DOF/lstm.py
@@ -17,7 +17,7 @@ class LSTM(object):
     Three gates regulate the information to be kept or discarded at each time step
     before passing on the long-term and short-term information to the next cell.
 
-    1. Input gate I_input uses two layers:
+    1. Input gate uses two layers:
         First layer I_1 selects what information can pass through it and what information to be discarded.
         Short-term memory and current input are passed into a sigmoid function. The output values
         are between 0 and 1, with 0 indicating unimportant data
@@ -34,7 +34,7 @@ class LSTM(object):
 
             input_gate = input_layer1 * input_layer2
 
-    2. Forget gate Ct keeps or discards information from the long-term memory.
+    2. Forget gate keeps or discards information from the long-term memory.
         First short-term memory and current input is passed through a sigmoid function again
 
             forget_gate = sigma(W_forget * (Ht_1, Xt) + bias_forget)
@@ -43,10 +43,29 @@ class LSTM(object):
 
             Ct = Ct_1 * forget_gate + input_gate
 
-    3. Output gate
-        Takes the current input Xt, the previous short-term memory Ht_1 (hidden state)
-        and long-term memory computed in current step and ouputs the new hidden state Ht
+    3. Output gate uses two layers:
+        Gate takes the current input Xt, the previous short-term memory Ht_1 (hidden state)
+        and long-term memory Ct computed in current step and ouputs the new hidden state Ht
 
+        First layer hort-term memory and current input is passed through a sigmoid function again
+
+            output_layer1 = sigma(W_output1 * (Ht_1, Xt) + bias_output1)
+
+        Second layer takes computed in current step long-term memory Ct and passes it
+        with it owns weights througt tahn activation function
+
+            output_layer2 = tanh(W_output2 * Ct + bias_output2))
+
+        New hidden state is the output of output gate:
+
+            output_gate = output_layer1 * output_layer2
+
+            Ht = output_gate
+
+    Short-term Ht and long-term memory Ct created by three gates will be passed over
+    to the next iteration and the whole process will be repeated.
+
+    The output on each interation can be accessed through hidden state Ht
 
 
     Parameters
