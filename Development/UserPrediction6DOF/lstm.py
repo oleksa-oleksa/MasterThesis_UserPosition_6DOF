827e4cb8aff3fce71b61d47bf932caba2931e2c0
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-02-03 12:21:02 +0100

    lstm doc strings network architecture description

--- Development/UserPrediction6DOF/lstm.py
+++ Development/UserPrediction6DOF/lstm.py
@@ -10,12 +10,43 @@ class LSTM(object):
     can handle the vanishing gradient problem faced by RNN.
 
     At each iteration, the LSTM uses gating mechanism and works with:
-    1. the current input data,
-    2. the hidden state aka the short-term memory
-    3. lastly the long-term memory.
+    1. the current input data Xt
+    2. the hidden state aka the short-term memory Ht_1
+    3. lastly the long-term memory Ct
 
-    Gates regulate the information to be kept or discarded at each time step
+    Three gates regulate the information to be kept or discarded at each time step
     before passing on the long-term and short-term information to the next cell.
+
+    1. Input gate I_input uses two layers:
+        First layer I_1 selects what information can pass through it and what information to be discarded.
+        Short-term memory and current input are passed into a sigmoid function. The output values
+        are between 0 and 1, with 0 indicating unimportant data
+        and 1 indicates that the information will be used.
+
+            input_layer1 = sigma(W_input1 * (Ht_1, Xt) + bias_input1)
+
+        Second layer takes same input information and passes it throught
+        tanh activation function with outputs values between -1.0 and 1.0.
+
+            input_layer2 = tanh(W_input2 * (Ht_1, Xt) + bias_input2))
+
+        I_input is the information to be kept in the long-term memory and used as the output.
+
+            input_gate = input_layer1 * input_layer2
+
+    2. Forget gate Ct keeps or discards information from the long-term memory.
+        First short-term memory and current input is passed through a sigmoid function again
+
+            forget_gate = sigma(W_forget * (Ht_1, Xt) + bias_forget)
+
+        New long-term memory is created from outputs of the Input gate and the Forget gate
+
+            Ct = Ct_1 * forget_gate + input_gate
+
+    3. Output gate
+        Takes the current input Xt, the previous short-term memory Ht_1 (hidden state)
+        and long-term memory computed in current step and ouputs the new hidden state Ht
+
 
 
     Parameters
