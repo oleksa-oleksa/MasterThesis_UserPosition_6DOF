bc24422201b089211eaaae7533b8796a0115eeed
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-04-20 18:43:12 +0200

    created singularity container on HHI Cluster

--- Development/UserPrediction6DOF.sh
+++ Development/UserPrediction6DOF.sh
@@ -3,6 +3,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=oleksandra.baga@hhi.fraunhofer.de
 
+#SBATCH --job-name=UserPrediction6DOF
+
 # stdout and stderr of this job will go into a file named like the job (%x) with SLURM_JOB_ID (5j)
 #SBATCH --output=gpu_outputs/%j_%x.out
 
@@ -22,5 +24,8 @@
 # Launch the singularity image with --nv for nvidia support.
 # The job writes its results to stdout which is directed to the output which starts with the job number file. Check it.
 
+# to build container write from ssh terminal
+# singularity build --force --fakeroot UserPrediction6DOF.sif UserPrediction6DOF.def
 singularity run --nv ./UserPrediction6DOF.sif
+
 
