a9656c24cb8364d3fe2c25193e43e82b96c60f3d
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-04-20 16:26:07 +0200

    ´GPU cluster service files are renamed

---
+++ Development/pred_6dof.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=oleksandra.baga@hhi.fraunhofer.de
+
+# stdout and stderr of this job will go into a file named like the job (%x) with SLURM_JOB_ID (5j)
+#SBATCH --output=%j_%x.out
+
+#SBATCH --nodes=1
+
+# ask slurm to run at most 1 task (slurm task == OS process) which might have subprocesses/threads
+#SBATCH --ntasks=1
+
+# number of cpus/task (threads/subprocesses). 8 is enough. 16 seems a reasonable max. with 4 GPUs on a 72 core machine.
+#SBATCH --cpus-per-task=8
+
+# request from the generic resources 1 GPU
+#SBATCH --gpus=1
+
+#SBATCH --mem=4G
+
+# Launch the singularity image with --nv for nvidia support.
+# The job writes its results to stdout which is directed to the output which starts with the job number file. Check it.
+
+singularity build --force --fakeroot ./pred_6dof.sif pred_6dof.def
+singularity run --nv ./pred_6dof.sif -e
