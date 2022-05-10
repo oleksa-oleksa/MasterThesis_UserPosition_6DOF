ae8eff94ce4a54e74b33731c4566b71433432dd7
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-05-10 13:28:58 +0200

    added cuda_path for function that logs model parameters

--- Development/UserPrediction6DOF/utils.py
+++ Development/UserPrediction6DOF/utils.py
@@ -271,7 +271,11 @@ def print_result(predictions, values):
 
 
 def log_parameters(hidden_dim, n_epochs, batch_size, df_results):
-    result_path = os.path.join(os.getcwd(), 'results')
+    results_path = ""
+    if torch.cuda.is_available():
+        results_path = "/mnt/output"
+    else:
+        result_path = os.path.join(os.getcwd(), 'results')
     csv_file = "model_parameters_adjust_log.csv"
     log_path = os.path.join(result_path, csv_file)
     csv_columns = ['LAT', 'hidden_size', 'epochs', 'batch_size', 'MSE_pos', 'MSE_rot', 'RMSE_pos', 'RMSE_rot']
