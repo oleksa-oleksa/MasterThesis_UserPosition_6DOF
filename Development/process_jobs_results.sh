b569b7b1f4151fcd77f0900824c3fad9d814fcc3
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-05-13 01:20:50 +0200

    got bash output from stdout

--- Development/process_jobs_results.sh
+++ Development/process_jobs_results.sh
@@ -6,10 +6,5 @@ echo "MSE_pos, MSE_rot, RMSE_pos, RMSE_rot, LAT, hidden_size, epochs, batch_size
 for file in "$process_dir"/*
 do
   out=$((tar -xOzf $file job_results/model_parameters_adjust_log.csv) 2>&1)
-
-  while IFS="," read -r MSE_pos MSE_rot RMSE_pos RMSE_rot LAT hidden_size epochs batch_size dropout
-  do
-      echo "$MSE_pos, $MSE_rot, $RMSE_pos, $RMSE_rot, $LAT, $hidden_size, $epochs, $batch_size, $dropout" >> results/model_parameters_merge.csv
-  done < <(tail -n +2 "$out")
+  tail -n +2 "$out"
 done
-#tar xf gpu_jobs_results/zz_533333.tar job_results/model_parameters_adjust_log.csv
