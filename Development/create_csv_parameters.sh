e38bb4a06b0d9bab067e5efce29b3a52946efd0c
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-05-12 10:19:27 +0200

    bash-script for creating csv-file for model's parameters adjustment

--- Development/create_csv_parameters.sh
+++ Development/create_csv_parameters.sh
@@ -3,10 +3,10 @@ echo "Bash version ${BASH_VERSION}..."
 for n_epochs in $(seq 200 100 1000)
     do
         for batch_size in $(seq 1 1 13)
-            for batch_size in $(seq 1 1 13)
-                do
+            do
+                for hidden_dim in $(seq 5 10 100)
                     do
-                        echo "n_epochs: $n_epochs, batch_size: $((2**$batch_size))"
+                        echo "n_epochs: $n_epochs, batch_size: $((2**$batch_size)), hidden_dim: $hidden_dim"
                     done
-                done
+            done
     done
