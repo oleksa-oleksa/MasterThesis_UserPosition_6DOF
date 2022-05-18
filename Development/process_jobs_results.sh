62aecbaebbce7001162e230d901045e50d9774cd
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-05-18 09:17:07 +0200

    experiment with averaged dataset

--- Development/process_jobs_results.sh
+++ Development/process_jobs_results.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-process_dir=gpu_jobs_results
+process_dir=gpu_jobs_results_interpolated
 
-echo "MSE_pos,MSE_rot,RMSE_pos,RMSE_rot,LAT,hidden_size,epochs,batch_size,dropout" > results/model_parameters_merge.csv
+echo "MSE_pos,MSE_rot,RMSE_pos,RMSE_rot,LAT,hidden_size,epochs,batch_size,dropout" > results/model_parameters_merge_interpolated_max.csv
 
 for file in "$process_dir"/*
 do
   out=$((tar -xOzf $file job_results/model_parameters_adjust_log.csv) >&1)
   log=$(echo -e "$out" | sed -n '2p')
-  echo $log >> results/model_parameters_merge.csv
+  echo $log >> results/model_parameters_merge_interpolated_max.csv
 done
