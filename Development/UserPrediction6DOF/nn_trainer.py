a1b6e4d4547bd5621d9ecad6cb9230397486be4b
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-08-02 20:38:22 +0200

    cpu() for cuda added

--- Development/UserPrediction6DOF/nn_trainer.py
+++ Development/UserPrediction6DOF/nn_trainer.py
@@ -38,7 +38,8 @@ class NNTrainer:
                     x_train_batch, y_train_batch = x_train_batch.cuda(), y_train_batch.cuda()
 
                 # print(f'x_train_batch: {x_train_batch.shape}')
-                y_train_batch = torch.tensor(np.round(y_train_batch.cpu().detach().numpy().cuda(), 8))
+                y_train_batch = torch.tensor(np.round(y_train_batch.cpu().detach().numpy(), 8))
+                y_train_batch = (torch.from_numpy(y_train_batch)).cuda()
 
                 outputs_train_batch = self.model.forward(x_train_batch)  # forward pass
                 # print(f'outputs_train_batch: {outputs_train_batch.shape}, y_train_batch: {y_train_batch.shape}')
@@ -70,6 +71,8 @@ class NNTrainer:
                     self.model.eval()
 
                     y_val_batch = torch.tensor(np.round(y_val_batch.numpy(), 8))
+                    y_val_batch = (torch.from_numpy(y_val_batch)).cuda()
+
                     y_val_hat = self.model(x_val_batch)
                     val_loss = self.criterion(y_val_batch, y_val_hat)
                     batch_val_losses.append(val_loss)
@@ -125,6 +128,8 @@ class NNTrainer:
                     x_test_batch, y_test_batch = x_test_batch.cuda(), y_test_batch.cuda()
 
                 y_test_batch = torch.tensor(np.round(y_test_batch.cpu().detach().numpy(), 8))
+                y_test_batch = (torch.from_numpy(y_test_batch)).cuda()
+
                 y_test_hat = self.model(x_test_batch)
                 test_loss = self.criterion(y_test_hat, y_test_batch)
                 batch_test_losses.append(test_loss)
