6fb6cccfe760b04b8b82960f7b28c684d9bda5e0
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-08-04 16:09:13 +0200

    added bash for launch of the tuning greed search

--- Development/Bash/run_tuning.sh
+++ Development/Bash/run_tuning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while IFS="," read -r hidden_dim batch_size lr_adam lr_epochs lr_multiplicator
+while IFS="," read -r hidden_dim batch_size lr_adam lr_epochs lr_multiplicator weight_decay_adam layers dropout n_epochs
 
 
 do
@@ -10,6 +10,10 @@ do
   export LR_ADAM=$lr_adam
   export LR_EPOCHS=$lr_epochs
   export LR_MULTIPLICATOR=$lr_multiplicator
+  export WEIGHT_DECAY_ADAM=$weight_decay_adam
+  export LAYERS=$layers
+  export DROPOUT=$dropout
+  export N_EPOCHS=$n_epochs
   export RNN_PARAMETERS=1
   nohup sbatch UserPrediction6DOF.sh &
 done < <(tail -n +2 $1)
