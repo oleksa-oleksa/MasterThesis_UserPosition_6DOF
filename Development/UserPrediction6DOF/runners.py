e0cbc263234626a493077f2f56b7a3adf48618f4
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-08-08 08:08:41 +0200

    predictions arrays 0.013 added for plotting

--- Development/UserPrediction6DOF/runners.py
+++ Development/UserPrediction6DOF/runners.py
@@ -287,17 +287,17 @@ class RNNRunner():
 
         # ---------  MODEL HYPERPARAMETERS ----------#
         self.reducing_learning_rate = True  # decreases LR every ls_epochs for 70%
-        self.learning_rate = 3e-4  # 1e-3 base Adam optimizer
-        self.lr_epochs = 50
-        self.lr_multiplicator = 0.3
+        self.learning_rate = 1e-4  # 1e-3 base Adam optimizer
+        self.lr_epochs = 30
+        self.lr_multiplicator = 0.5
         self.weight_decay = 1e-12  # 1e-6 base Adam optimizer
 
         # self.num_past = 20  # number of past time series to predict future
         self.input_dim = len(self.features)
         self.output_dim = len(self.outputs)  # 3 position parameter + 4 rotation parameter
-        self.hidden_dim = 128  # number of features in hidden state
+        self.hidden_dim = 512  # number of features in hidden state
         self.batch_size = 128	
-        self.n_epochs = 200
+        self.n_epochs = 500
         self.dropout = 0
         self.layer_dim = 1  # the number of LSTM layers stacked on top of each other
         self.seq_length_input = 20  # input length of timeseries from the past
@@ -511,7 +511,7 @@ class RNNRunner():
         nn_train.train(train_loader, val_loader, self.n_epochs)
 
         # Plot train and validation losses
-        self.plotter.plot_losses(nn_train.train_losses, nn_train.val_losses, self.params, self.results_path)
+        # self.plotter.plot_losses(nn_train.train_losses, nn_train.val_losses, self.params, self.results_path)
 
         # PREDICTION ON TEST DATA
         logging.info('Training finished. Starting prediction on test data!')
