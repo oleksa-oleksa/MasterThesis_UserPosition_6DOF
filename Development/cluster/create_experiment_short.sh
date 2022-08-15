5662b992e795e5e7b42fa48bea2f85c02c4051e8
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-08-15 14:48:33 +0200

    created short experiment to run GRU modifications

---
+++ Development/cluster/create_experiment_short.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+out=experiment_parameters_short.csv
+
+echo "hidden_dim,batch_size,lr_adam,lr_epochs,lr_multiplicator" > $out
+
+for hidden_dim in $(seq 9 1 10)
+    do
+        for batch_size in $(seq 8 1 10)
+        do
+            for lr_adam in $(seq 0.00010 0.0001 0.0003)
+            do
+                for lr_epochs in $(seq 40 10 80)
+                do
+                    for lr_multiplicator in $(seq 0.3 0.1 0.7)
+                    do
+                        echo "$((2**$hidden_dim)),$((2**$batch_size)),$lr_adam,$lr_epochs,$lr_multiplicator" >> $out
+                    done
+                done
+            done
+        done
+    done
+echo "$out is written"
