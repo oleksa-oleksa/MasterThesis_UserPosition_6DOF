54d1389deb4be841b4a14ee7635651092ff8f462
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-08-27 08:19:12 +0200

    GRU31 results

--- Development/UserPrediction6DOF.sh
+++ Development/UserPrediction6DOF.sh
@@ -21,7 +21,7 @@
 # to build container write from ssh terminal
 # singularity build --force --fakeroot UserPrediction6DOF.sif UserPrediction6DOF.def
 
-OUT_DIR=jobs_results_gru3_final
+OUT_DIR=jobs_results_gru31_final
 
 source "/etc/slurm/local_job_dir.sh"
 mkdir -p "${LOCAL_JOB_DIR}/job_results"
