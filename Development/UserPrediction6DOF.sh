eecc2130e8b25f9345adb4e714c4fb16a3f9ddea
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-08-29 21:52:17 +0200

    tar

--- Development/UserPrediction6DOF.sh
+++ Development/UserPrediction6DOF.sh
@@ -21,7 +21,7 @@
 # to build container write from ssh terminal
 # singularity build --force --fakeroot UserPrediction6DOF.sif UserPrediction6DOF.def
 
-OUT_DIR=gru-bi1
+OUT_DIR=tune
 
 source "/etc/slurm/local_job_dir.sh"
 mkdir -p "${LOCAL_JOB_DIR}/job_results"
