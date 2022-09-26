b53eb78fa24c7b9a251d9a4c75cdd5aae3851d6b
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-26 10:45:37 +0200

    Section -GRU Model-: added GRU1 architecture and short analysis of a model

--- Development/UserPrediction6DOF/runners.py
+++ Development/UserPrediction6DOF/runners.py
@@ -148,7 +148,7 @@ class RNNRunner:
         self.input_dim = len(self.features)
         self.output_dim = len(self.outputs)  # 3 position parameter + 4 rotation parameter
         self.hidden_dim = 512  # number of features in hidden state
-        self.batch_size = 128
+        self.batch_size = 512
         self.n_epochs = 500
         self.seq_length_input = 20  # input length of timeseries from the past
 
